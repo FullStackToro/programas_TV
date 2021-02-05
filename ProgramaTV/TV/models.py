620a706d9213e8067d6ce5f06c7369ac451c3c2c
FullStackToro <g.toro.guillon@gmail.com>
2021-02-04 23:34:11 -0300

    Se agregan validaciones en Django

--- ProgramaTV/TV/models.py
+++ ProgramaTV/TV/models.py
@@ -1,9 +1,57 @@
 from django.db import models
+from datetime import datetime
+from time import gmtime, strftime
+
+
+class Libro_manager(models.Manager):
+    def validacion(self, postData):
+        error = {}
+        time = datetime.strptime(postData['fecha_1'], '%Y-%m-%d')
+        now=datetime.now()
+        largo_data = [2, 3, 10]
+        if len(postData['titulo_1']) < largo_data[0]:
+            error['titulo_1']=f"El título debe tener {largo_data[0]} caracteres"
+        if len(postData['net_1']) < largo_data[1]:
+            error['net_1'] = f"El network debe tener {largo_data[1]} caracteres"
+        if time > datetime.now():
+            error['net_1'] = f"La fecha no puede ser superior a {now.year}/{now.month}/{now.day}"
+        data=postData['desc']
+        print(len(data.replace(" ","")))
+        if len(data.replace(" ","")) < largo_data[2]:
+            if postData['desc'] is None:
+                pass
+            else:
+                error['desc'] = f"La descripción debe tener {largo_data[2]} caracteres"
+        return error
+
+
+    def validacion_new(self, postData):
+        error = {}
+        largo_data = [2, 3, 10]
+        if len(postData['titulo_1']) < largo_data[0]:
+            error['titulo_1'] = f"El título debe tener {largo_data[0]} caracteres"
+        if len(postData['net_1']) < largo_data[0]:
+            error['net_1'] = f"El network debe tener {largo_data[1]} caracteres"
+        if len(postData['desc']) < largo_data[0]:
+            if postData['desc'] is None:
+                pass
+            else:
+                error['desc'] = f"La descripción debe tener {largo_data[2]} caracteres"
+        unique_title = Libro.objects.all()
+        for libro in unique_title:
+            if postData['titulo_1'].lower() == libro.title.lower():
+                error['title2'] = f"El nombre {libro.title} ya existe"
+        return error
+
+
 
 class Libro(models.Model):
     title = models.CharField(max_length=255)
     netword = models.CharField(max_length=255)
     release_date = models.DateTimeField()
-    description = models.TextField()
+    description = models.TextField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = Libro_manager()
+
+
--- ProgramaTV/TV/views.py
+++ ProgramaTV/TV/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render, redirect
 from TV.models import Libro
+from django.contrib import messages
+from time import gmtime, strftime
 from datetime import datetime
 
 def new(request):
     return render(request, "index.html")
 
 def create(request):
-    op = Libro.objects.create(title=request.POST['t1'], netword=request.POST['c1'], release_date=request.POST['f1'], description=request.POST['d1'])
-    return redirect(f'/shows/{op.id}')
+    error=Libro.objects.validacion_new(request.POST)
+    if len(error)>0:
+        for key, value in error.items():
+            messages.error(request, value)
+            print(key, value)
+        return redirect('/shows/new')
+    else:
+        op = Libro.objects.create(title=request.POST['titulo_1'], netword=request.POST['net_1'], release_date=request.POST['fecha_1'], description=request.POST['desc'])
+        return redirect(f'/shows/{op.id}')
 # Create your views here.
 
 def show(request):
@@ -15,7 +24,6 @@ def show(request):
     export=[]
     for u in libro:
         export.append({'id': u.id, 'titulo': u.title, 'Network': u.netword, "Fecha de Estreno": u.release_date})
-        print(u.id, u.title, '\n'*2, '-'*40, '\n')
 
     context = {
         'libros': export
@@ -23,11 +31,19 @@ def show(request):
     return render(request, "show_all.html", context)
 
 def update(request,op):
-    libro = Libro.objects.get(id=int(op))
-    libro.netword=request.POST['n']
-    libro.title=request.POST['t']
-    libro.release_date=request.POST['f']
-    libro.save()
+    error=Libro.objects.validacion(request.POST)
+    if len(error)>0:
+        for key, value in error.items():
+            messages.error(request, value)
+            print(key, value)
+        return redirect(f'/shows/{op}/edit')
+    else:
+        libro = Libro.objects.get(id=int(op))
+        libro.title = request.POST['titulo_1']
+        libro.netword = request.POST['net_1']
+        libro.release_date = request.POST['fecha_1']
+        libro.description = request.POST['desc']
+        libro.save()
     return redirect(f"/shows/{libro.id}")
 
 
