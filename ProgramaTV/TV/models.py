b3c1125cc02bd22bbae1a0457e8bef0e98dd042d
FullStackToro <g.toro.guillon@gmail.com>
2021-02-04 23:36:21 -0300

    se modifica validación de fecha en nuevo libro

--- ProgramaTV/TV/models.py
+++ ProgramaTV/TV/models.py
@@ -27,12 +27,15 @@ class Libro_manager(models.Manager):
 
     def validacion_new(self, postData):
         error = {}
+        time = datetime.strptime(postData['fecha_1'], '%Y-%m-%d')
+        now=datetime.now()
         largo_data = [2, 3, 10]
         if len(postData['titulo_1']) < largo_data[0]:
             error['titulo_1'] = f"El título debe tener {largo_data[0]} caracteres"
-        if len(postData['net_1']) < largo_data[0]:
+        if len(postData['net_1']) < largo_data[1]:
             error['net_1'] = f"El network debe tener {largo_data[1]} caracteres"
-        if len(postData['desc']) < largo_data[0]:
+
+        if len(postData['desc']) < largo_data[2]:
             if postData['desc'] is None:
                 pass
             else:
@@ -41,6 +44,16 @@ class Libro_manager(models.Manager):
         for libro in unique_title:
             if postData['titulo_1'].lower() == libro.title.lower():
                 error['title2'] = f"El nombre {libro.title} ya existe"
+
+        if time > datetime.now():
+            error['net_1'] = f"La fecha no puede ser superior a {now.year}/{now.month}/{now.day}"
+        data = postData['desc']
+        print(len(data.replace(" ","")))
+        if len(data.replace(" ","")) < largo_data[2]:
+            if postData['desc'] is None:
+                pass
+            else:
+                error['desc'] = f"La descripción debe tener {largo_data[2]} caracteres"
         return error
 
 
